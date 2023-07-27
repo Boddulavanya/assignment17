c0def7bff9bcd75a2ba17bb7394431acf4efe536
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-07-27 16:03:34 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assignment_17.circle;
+using static Assignment_17.rectangle;
+using static Assignment_17.triangle;
+
+namespace Assignment_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(3);
+            circle.PrintInfo();
+
+            Rectangle rectangle = new Rectangle(6, 4);
+            rectangle.PrintInfo();
+
+            Triangle triangle = new Triangle(2, 4, 7);
+            triangle.PrintInfo();
+        }
+    }
+}
+
---
+++ shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_17
+{
+    internal class shape
+    {
+        public class Shape
+        {
+            public string ShapeType { get; set; }
+
+            public virtual double Area { get; }
+            public virtual double Perimeter { get; }
+        }
+
+    }
+}
+
